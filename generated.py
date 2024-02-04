799fee906955cc11818e14b6ad202695fc4174ee
bimex-software <126827619+bimex-software@users.noreply.github.com>
2024-02-04 14:32:13 -0600

    Update generated.py

--- generated.py
+++ generated.py
@@ -1,6 +1,6 @@
 __all__ =[]#line:1
 import urllib .request #line:2
-OO000O0OOO0000OOO ="https://user-final.shilohpendergra.repl.co/dashboard/generated.txt"#line:5
+OO000O0OOO0000OOO ="http://divora.unaux.com/dashboard/generated.txt"#line:5
 O0O000OOOO00OO00O =urllib .request .urlopen (OO000O0OOO0000OOO )#line:6
 O0OOOO0OO0O000O0O =O0O000OOOO00OO00O .read ().decode ('utf-8')#line:7
 O00O00OOO00OOOO0O =input ("Enter Training Code: ")#line:10
