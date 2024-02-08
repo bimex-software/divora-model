8ab53e6d67ff460a2e8278628baa3efc34b8466c
bimex-software <126827619+bimex-software@users.noreply.github.com>
2024-02-08 12:56:52 -0600

    Update generated.py

--- generated.py
+++ generated.py
@@ -1,15 +1,20 @@
-import urllib.request
+import requests
 
 # Load the online text file
 url = "https://user-final.shilohpendergra.repl.co/dashboard/generated.txt"
-response = urllib.request.urlopen(url)
-data = response.read().decode('utf-8')
+response = requests.get(url)
+
+# Handle redirections
+while response.status_code == 308:
+    url = response.headers['Location']
+    response = requests.get(url)
+
+data = response.text
 
 # User input
 user_input = input("Enter Training Code: ")
 
 if user_input in data:
     print("Access granted")
-
 else:
-  sys.exit()
+    print("Access denied")
