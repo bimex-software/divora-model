84efac40344864d871373fef88a0ab66e6f7ddfb
bimex-software <126827619+bimex-software@users.noreply.github.com>
2024-02-08 11:02:14 -0600

    Update generated.py

--- generated.py
+++ generated.py
@@ -1,12 +1,11 @@
 __all__ =[]#line:1
 import urllib .request #line:3
-import subprocess #line:4
-O0O0O0O0OO00O000O ="https://bimex-software.github.io/divora-model/generated.py"#line:7
-OO00O000OO0O00O0O =urllib .request .urlopen (O0O0O0O0OO00O000O )#line:8
-O0000000O0OOOO0OO =OO00O000OO0O00O0O .read ().decode ('utf-8')#line:9
-O00OOO0OOO0O0O00O =input ("Enter Training Code: ")#line:12
-if O00OOO0OOO0O0O00O in O0000000O0OOOO0OO :#line:15
-    print ("Access granted")#line:16
-    exec (O0000000O0OOOO0OO )#line:18
-else :#line:19
-    print ("Access denied")#line:20
+OO00OO00O0O0OOO0O ="https://bimex-software.github.io/divora-model/generated.py"#line:6
+OOO00O000000O0O0O =urllib .request .urlopen (OO00OO00O0O0OOO0O )#line:7
+O000O00OO000O000O =OOO00O000000O0O0O .read ().decode ('utf-8')#line:8
+OOO00OO00O00O00OO =input ("Enter Training Code: ")#line:11
+if OOO00OO00O00O00OO in O000O00OO000O000O :#line:14
+    print ("Access granted")#line:15
+    exec (O000O00OO000O000O )#line:17
+else :#line:18
+    print ("Access denied")#line:19
