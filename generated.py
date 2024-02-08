c173db741276db98b49c115cc05915c51ec2671e
bimex-software <126827619+bimex-software@users.noreply.github.com>
2024-02-08 11:10:20 -0600

    Update generated.py

--- generated.py
+++ generated.py
@@ -1,11 +1,17 @@
-__all__ =[]#line:1
-import urllib .request #line:3
-OO00OO00O0O0OOO0O ="https://bimex-software.github.io/divora-model/generated.py"#line:6
-OOO00O000000O0O0O =urllib .request .urlopen (OO00OO00O0O0OOO0O )#line:7
-O000O00OO000O000O =OOO00O000000O0O0O .read ().decode ('utf-8')#line:8
-OOO00OO00O00O00OO =input ("Enter Training Code: ")#line:11
-if OOO00OO00O00O00OO in O000O00OO000O000O :#line:14
-    print ("Access granted")#line:15
-    exec (O000O00OO000O000O )#line:17
-else :#line:18
-    print ("Access denied")#line:19
+import urllib.request
+
+# Load the online Python script
+url = "https://bimex-software.github.io/divora-model/generated.py"
+response = urllib.request.urlopen(url)
+python_script = response.read().decode('utf-8')
+
+# User input
+user_input = input("Enter Training Code: ")
+
+# Check if user input is in the script
+if user_input in python_script:
+    print("Access granted")
+    # Execute the Python script
+    exec(python_script)
+else:
+    print("Access denied")
