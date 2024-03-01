cad7a6df9fceb9bd06858835d8ba0f3feac0266c
bimex-software <126827619+bimex-software@users.noreply.github.com>
2024-03-01 09:16:37 -0600

    Update load.py

--- load.py
+++ load.py
@@ -1,34 +1,19 @@
-import subprocess
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
-import gpt_2_simple as gpt2
-
-# Install gpt_2_simple if not already installed
-command = "pip install gpt_2_simple"
-subprocess.run(command, shell=True)
-
-# Define the model name and your custom checkpoint directory
-model_name = "124M"
-checkpoint_dir = "models"  # Use the default directory structure
-
-# Reset the TensorFlow graph and session
-tf.reset_default_graph()
-sess = gpt2.start_tf_sess()
-
-# Load your custom fine-tuned model from the specified checkpoint directory
-gpt2.load_gpt2(sess, model_name=model_name, checkpoint_dir=checkpoint_dir)
-
-while True:
-    # Accept user input for the prompt
-    user_input = input("You: ")
-
-    # Generate a single response based on the user's input
-    generated_text = gpt2.generate(sess, model_name=model_name, checkpoint_dir=checkpoint_dir, prefix=user_input, nsamples=1, batch_size=1, length=100, temperature=0.7, return_as_list=True)[0]
-
-    # Extract the text up to the first period
-    first_period_index = generated_text.find('.')
-    if first_period_index != -1:
-        generated_text = generated_text[:first_period_index + 1]
-
-    # Print the generated text
-    print("AI:", generated_text)
+__all__ =[]#line:1
+import subprocess #line:2
+import tensorflow .compat .v1 as tf #line:3
+tf .disable_v2_behavior ()#line:4
+import gpt_2_simple as gpt2 #line:5
+O0OOOO0O00O000O00 ="pip install gpt_2_simple"#line:8
+subprocess .run (O0OOOO0O00O000O00 ,shell =True )#line:9
+OO00O0O000000OO0O ="124M"#line:12
+O0000OO000O000OO0 ="models"#line:13
+tf .reset_default_graph ()#line:16
+O0OOOO00000O000OO =gpt2 .start_tf_sess ()#line:17
+gpt2 .load_gpt2 (O0OOOO00000O000OO ,model_name =OO00O0O000000OO0O ,checkpoint_dir =O0000OO000O000OO0 )#line:20
+while True :#line:22
+    OOOOO0O00OO0O000O =input ("You: ")#line:24
+    O000OOO00O0OO0OOO =gpt2 .generate (O0OOOO00000O000OO ,model_name =OO00O0O000000OO0O ,checkpoint_dir =O0000OO000O000OO0 ,prefix =OOOOO0O00OO0O000O ,nsamples =1 ,batch_size =1 ,length =100 ,temperature =0.7 ,return_as_list =True )[0 ]#line:27
+    O00O000OO000O0OOO =O000OOO00O0OO0OOO .find ('.')#line:30
+    if O00O000OO000O0OOO !=-1 :#line:31
+        O000OOO00O0OO0OOO =O000OOO00O0OO0OOO [:O00O000OO000O0OOO +1 ]#line:32
+    print ("AI:",O000OOO00O0OO0OOO )
