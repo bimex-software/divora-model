c907fdb0f426d266adab23d3e77164bc72fd29c7
bimex-software <126827619+bimex-software@users.noreply.github.com>
2024-03-12 09:06:39 -0500

    Update load.py

--- load.py
+++ load.py
@@ -11,9 +11,9 @@ tf .reset_default_graph ()#line:16
 O0OOOO00000O000OO =gpt2 .start_tf_sess ()#line:17
 gpt2 .load_gpt2 (O0OOOO00000O000OO ,model_name =OO00O0O000000OO0O ,checkpoint_dir =O0000OO000O000OO0 )#line:20
 while True :#line:22
-    OOOOO0O00OO0O000O =input ("You: ")#line:24
+    OOOOO0O00OO0O000O =input ("instructions")#line:24
     O000OOO00O0OO0OOO =gpt2 .generate (O0OOOO00000O000OO ,model_name =OO00O0O000000OO0O ,checkpoint_dir =O0000OO000O000OO0 ,prefix =OOOOO0O00OO0O000O ,nsamples =1 ,batch_size =1 ,length =100 ,temperature =0.7 ,return_as_list =True )[0 ]#line:27
     O00O000OO000O0OOO =O000OOO00O0OO0OOO .find ('.')#line:30
     if O00O000OO000O0OOO !=-1 :#line:31
         O000OOO00O0OO0OOO =O000OOO00O0OO0OOO [:O00O000OO000O0OOO +1 ]#line:32
-    print ("AI:",O000OOO00O0OO0OOO )
+    print ("output",O000OOO00O0OO0OOO )
