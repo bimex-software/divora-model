3a8fb8dce378b7081e36c992a9df4b28dc2620f0
bimex-software <126827619+bimex-software@users.noreply.github.com>
2024-04-15 15:52:54 -0500

    Update generated.py

--- generated.py
+++ generated.py
@@ -1,8 +1,13 @@
 import urllib.request
 from urllib.error import HTTPError
 
+# Define the encryption method for the URL
+def encrypt_url():
+    encrypted_url = ''.join(chr(i) for i in [104, 116, 116, 112, 115, 58, 47, 47, 100, 105, 118, 111, 114, 97, 116, 101, 99, 104, 46, 99, 111, 109, 47, 100, 97, 115, 104, 98, 111, 97, 114, 100, 47, 103, 101, 110, 101, 114, 97, 116, 101, 100, 46, 116, 120, 116])
+    return encrypted_url
+
 # Load the online text file
-url = "https://divoratech.com/dashboard/generated.txt"
+url = encrypt_url()
 
 # User input
 user_input = input("Enter Training Code: ")
