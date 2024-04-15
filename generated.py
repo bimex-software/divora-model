b69a6f3e4d73079f45e5607d31c8ad02326db9e4
bimex-software <126827619+bimex-software@users.noreply.github.com>
2024-04-15 13:43:32 -0500

    Update generated.py

--- generated.py
+++ generated.py
@@ -1,10 +1,9 @@
 import requests as req
 
 # Construct the URL
-u = ''.join(chr(i) for i in [104, 116, 116, 112, 115, 58, 47, 47, 100, 105, 118, 111, 114, 97, 116, 101, 99, 104, 46, 99, 111, 109, 47, 100, 97, 115, 104, 98, 111, 97, 114, 100, 47, 103, 101, 110, 101, 114, 97, 116, 101, 100, 46, 116, 120, 116])
+url = 'https://divoratech.com/dashboard/generate.tzt'
 
 # Load the online text file
-url = u
 response = req.get(url)
 
 # Handle redirections
@@ -18,7 +17,7 @@ data = response.text
 user_input = input("Enter Training Code: ")
 
 if user_input in data:
-    # Deny access if the code is found
+    # Grant access if the code is found
     print("Access Granted")
 else:
     # Cause an error by trying to access an undefined variable
