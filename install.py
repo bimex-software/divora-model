6e403b7edfe78dfc2c4723edb8d83686db914f06
bimex-software <126827619+bimex-software@users.noreply.github.com>
2024-04-15 13:47:27 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
-__all__ =[]#line:1
-import subprocess #line:3
-OOOOO000O00O00O0O ="pip install gpt_2_simple"#line:5
-subprocess .run (OOOOO000O00O00O0O ,shell =True )#line:6
+import subprocess
+
+command = "pip install gpt_2_simple"
+subprocess.run(command, shell=True)
